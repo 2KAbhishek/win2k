42c3e034363deda19f86eeaf65e82a2508009049
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-31 03:48:51 +0530

    Add psreadline and psfzf

--- WindowsPowerShell/Setup.ps1
+++ WindowsPowerShell/Setup.ps1
@@ -1,3 +1,5 @@
 cp ~/Projects/Winfiles/WindowsPowerShell/* ~/Documents/WindowsPowerShell/
 # Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 # Install-Module -Name z -Force -AllowClobber
+# Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+# Install-Module -Name PSFzf -Scope CurrentUser -Force
