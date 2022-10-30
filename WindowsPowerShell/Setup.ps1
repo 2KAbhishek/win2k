b98bdcbd7cb51b1f46502e903dfb31affecc1bb9
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-31 04:18:21 +0530

    Add powershell 7

--- WindowsPowerShell/Setup.ps1
+++ WindowsPowerShell/Setup.ps1
@@ -1,4 +1,5 @@
-cp ~/Projects/Winfiles/WindowsPowerShell/* ~/Documents/WindowsPowerShell/
+cp ~/Projects/Winfiles/PowerShell/* ~/Documents/WindowsPowerShell/ # PowerShell 5.1
+cp ~/Projects/Winfiles/PowerShell/* ~/Documents/PowerShell/ #PowerShell 7
 # Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 # Install-Module -Name z -Force -AllowClobber
 # Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
