1c00e0b2a8a455b9032a7c9e3b48d143506f4b40
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-31 03:18:17 +0530

    Add setup command

---
+++ WindowsPowerShell/Setup.ps1
@@ -0,0 +1,3 @@
+cp ~/Projects/Winfiles/WindowsPowerShell/* ~/Documents/WindowsPowerShell/
+# Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+# Install-Module -Name z -Force -AllowClobber
