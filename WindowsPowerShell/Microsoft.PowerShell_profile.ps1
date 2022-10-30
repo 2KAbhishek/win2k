d13bb7c7789f589c3ed1a73b6cbeb9348974a4ae
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-10-31 03:20:59 +0530

    Cleanup

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,12 @@
 # File for Current User, Current Host - $PROFILE.CurrentUserCurrentHost
-$Env:KOMOREBI_CONFIG_HOME = '$Home\.komorebi'
-$Env:KOMOREBI_AHK_V2_EXE = 'C:\Program Files\AutoHotkey\v2.0-beta.12\AutoHotkey64.exe'
 
-function Startup {
+function TileWindows {
+    # $Env:KOMOREBI_CONFIG_HOME = '$HOME\.komorebi'
+    # $Env:KOMOREBI_AHK_V1_EXE = 'C:\Program Files\AutoHotkey\v1.1.34.04\AutoHotkeyU64.exe'
+    # $Env:KOMOREBI_AHK_V2_EXE = 'C:\Program Files\AutoHotkey\v2.0-beta.12\AutoHotkey64.exe'
     Start-Process komorebi.exe -WindowStyle hidden
-    Start-Process '$Env:KOMOREBI_AHK_V2_EXE' '$Env:KOMOREBI_CONFIG_HOME\mappings.ahk'
-    Start-Process python.exe '$Home\Projects\yasb\src\main.py' -WindowStyle hidden
+    # Start-Process 'C:\Program Files\AutoHotkey\v2.0-beta.12\AutoHotkey64.exe' 'C:\Users\DELL\komorebi.ahk'
+    # Start-Process python.exe $Home\Projects\yasb\src\main.py  -WindowStyle hidden
 }
 
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
