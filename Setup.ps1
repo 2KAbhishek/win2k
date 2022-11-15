5fa6efd87bfb10177e891dc28a62abb8d09713d1
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-11-15 23:15:53 +0530

    Update setup script

--- Setup.ps1
+++ Setup.ps1
@@ -1,14 +1,15 @@
-# Scoop
-scoop bucket add anderlli0053_DEV-tools https://github.com/anderlli0053/DEV-tools
-scoop install winget powershell oh-my-posh powertoys 7zip ag bat delta exa fasd fd fzf gh git gzip lazygit less lsd msys ntop navi openssh ripgrep vifm wget nodejs16 python
+# Scoop Packages
+ scoop bucket add anderlli0053_DEV-tools https://github.com/anderlli0053/DEV-tools
+ scoop install winget powershell oh-my-posh powertoys 7zip ag bat delta exa fasd fd fzf gh git gzip lazygit less lsd msys ntop navi openssh ripgrep vifm wget nodejs16 python
 
-# Powershell
-cp ~/Projects/Winfiles/PowerShell/* ~/Documents/WindowsPowerShell/ # PowerShell 5.1
-cp ~/Projects/Winfiles/PowerShell/* ~/Documents/PowerShell/ #PowerShell 7
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 Install-Module -Name z -Force -AllowClobber
 Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name PSFzf -Scope CurrentUser -Force
 
+# Powershell
+cmd .c mklink /d %HOMEPATH%\Documents\WindowsPowerShell %HOMEPATH%\Projects\Winfiles\PowerShell\
+# Powershell 7
+cmd /c mklink /d %HOMEPATH%\Documents\PowerShell\ %HOMEPATH%\Projects\Winfiles\PowerShell\
 # Terminal
-cp ~/Projects/Winfiles/Terminal/* ~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/
+cmd /c mklink /d %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ %HOMEPATH%\Projects\Winfiles\Terminal\
