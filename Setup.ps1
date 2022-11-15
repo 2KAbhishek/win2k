278837a7ace18541d544d18d5a91cb03d6f550ad
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-11-16 01:15:31 +0530

    Update setup script

--- Setup.ps1
+++ Setup.ps1
@@ -1,6 +1,6 @@
 # Scoop Packages
- scoop bucket add anderlli0053_DEV-tools https://github.com/anderlli0053/DEV-tools
- scoop install winget powershell oh-my-posh powertoys 7zip ag bat delta exa fasd fd fzf gh git gzip lazygit less lsd msys ntop navi openssh ripgrep vifm wget nodejs16 python
+scoop bucket add anderlli0053_DEV-tools https://github.com/anderlli0053/DEV-tools
+scoop install winget powershell oh-my-posh powertoys 7zip ag bat delta exa fasd fd fzf gh git gzip lazygit less lsd msys ntop navi openssh ripgrep vifm wget nodejs16 python
 
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 Install-Module -Name z -Force -AllowClobber
@@ -8,8 +8,10 @@ Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name PSFzf -Scope CurrentUser -Force
 
 # Powershell
-cmd .c mklink /d %HOMEPATH%\Documents\WindowsPowerShell %HOMEPATH%\Projects\Winfiles\PowerShell\
+cmd /c mklink /d %HOMEPATH%\Documents\WindowsPowerShell %CD%\PowerShell\
 # Powershell 7
-cmd /c mklink /d %HOMEPATH%\Documents\PowerShell\ %HOMEPATH%\Projects\Winfiles\PowerShell\
+cmd /c mklink /d %HOMEPATH%\Documents\PowerShell\ %CD%\PowerShell\
+# Posh2K
+cmd /c mklink /d %HOMEPATH%\Documents\Posh2K %CD%\Posh2K\
 # Terminal
-cmd /c mklink /d %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ %HOMEPATH%\Projects\Winfiles\Terminal\
+cmd /c mklink /d %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ %CD%\Terminal\
