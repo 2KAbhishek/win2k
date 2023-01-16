9ad6665c0dd339bcc62fa0980a4fbfeee6670653
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-01-16 09:21:10 +0530

    move: all configs to config folder

--- setup.ps1
+++ setup.ps1
@@ -5,19 +5,19 @@ fd fzf gh git gzip lazygit less lsd make msys ntop navi openssh ripgrep vifm wge
 
 scoop update *
 
-Install-Module -Name Terminal-Icons -Repository PSGallery -Force -AllowClobber
-Install-Module -Name z -Force -AllowClobber
+Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser -Force -AllowClobber
+Install-Module -Name z -Scope CurrentUser -Force -AllowClobber
 Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck -AllowClobber
 Install-Module -Name PSFzf -Scope CurrentUser -Force -AllowClobber
 
-Update-Module
+Update-Module -Scope CurrentUser
 
 # Powershell
-cmd /c mklink /d %HOMEPATH%\Documents\WindowsPowerShell %CD%\PowerShell\
+cmd /c mklink /d %HOMEPATH%\Documents\WindowsPowerShell %CD%\.config\PowerShell\
 # Powershell 7
-cmd /c mklink /d %HOMEPATH%\Documents\PowerShell\ %CD%\PowerShell\
+cmd /c mklink /d %HOMEPATH%\Documents\PowerShell\ %CD%\.config\PowerShell\
 
-cmd /c mklink /d %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ %CD%\Terminal\
+cmd /c mklink /d %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ %CD%\.config\Terminal\
 
 # Fetch submodules
 git submodule update --init
