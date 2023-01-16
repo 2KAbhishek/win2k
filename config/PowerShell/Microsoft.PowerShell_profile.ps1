172d30463522f3f9dd83bf13c62cd1872e930cb6
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-01-16 10:49:42 +0530

    feat: start and stop tiling commands

--- config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,16 @@
 # File for Current User, Current Host - $PROFILE.CurrentUserCurrentHost
 
-function TileWindows {
+function StartTiling {
     # $Env:KOMOREBI_CONFIG_HOME = '$HOME\.komorebi'
     # $Env:KOMOREBI_AHK_V1_EXE = 'C:\Program Files\AutoHotkey\v1.1.34.04\AutoHotkeyU64.exe'
     # $Env:KOMOREBI_AHK_V2_EXE = 'C:\Program Files\AutoHotkey\v2.0-beta.12\AutoHotkey64.exe'
     Start-Process komorebi.exe -WindowStyle hidden
     # Start-Process 'C:\Program Files\AutoHotkey\v2.0-beta.12\AutoHotkey64.exe' 'C:\Users\DELL\komorebi.ahk'
     # Start-Process python.exe $Home\Projects\yasb\src\main.py  -WindowStyle hidden
+}
+
+function StopTiling {
+    taskkill /f /im komorebi.exe
 }
 
 oh-my-posh init pwsh --config "$HOME/Documents/Posh2K/posh2k.omp.json" | Invoke-Expression
