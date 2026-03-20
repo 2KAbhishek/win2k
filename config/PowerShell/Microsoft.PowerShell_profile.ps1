f57c4f6998993fb2d9bc9938eb2be680d3030e8e
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-03-20 15:56:14 +0530

    feat: remove komorebi functions

--- config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,3 @@
 # File for Current User, Current Host - $PROFILE.CurrentUserCurrentHost
 
-function StartTiling {
-    komorebic start -c "$Env:USERPROFILE\.config\komorebi\komorebi.json" --whkd
-}
-
-function StopTiling {
-    taskkill /f /im komorebi.exe
-}
-
 oh-my-posh init pwsh --config "$HOME/Documents/posh2k/posh2k.omp.json" | Invoke-Expression
