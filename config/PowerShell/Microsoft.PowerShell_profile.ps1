417a6edb4283081da39aa350f8e4f53e4a721b3d
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-04-18 17:33:10 +0530

    feat: update oh-my-posh config name

--- config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 # File for Current User, Current Host - $PROFILE.CurrentUserCurrentHost
 
-oh-my-posh init pwsh --config "$HOME/Documents/posh2k/posh2k.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$HOME/Documents/posh2k/posh2k.toml" | Invoke-Expression
 
 $localPwsh = Join-Path $env:USERPROFILE 'local.ps1'
 if (Test-Path -LiteralPath $localPwsh) {
